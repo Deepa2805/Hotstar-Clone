be43f6a5e444449fa0d68ea40f4adc350cf9f843
deepa2805 <dipmala2805@gmail.com>
2023-03-28 21:28:47 +0530

    Hotstar-Clone

---
+++ data.js
@@ -0,0 +1,37 @@
+let movies =[
+ {
+    name: "Pop Kaun",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, commodi. Beatae optio tempore repellendus praesentium."
+    images: "images/Pop Kaun img.webp"
+},
+
+{
+    name: "Teri Meri Doriyan",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, commodi. Beatae optio tempore repellendus praesentium."
+    images: "images/Teri Meri Doriyan img.webp"
+},
+
+{
+    name: "The Legend",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, commodi. Beatae optio tempore repellendus praesentium."
+    images: "images/The Legend img.webp"
+},
+
+{
+    name: "The Night Manager",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, commodi. Beatae optio tempore repellendus praesentium."
+    images: "images/The Night Manager img.webp"
+},
+
+{
+    name: "Cuttputlli",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, commodi. Beatae optio tempore repellendus praesentium."
+    images: "images/Cuttputlli.webp"
+},
+
+{
+    name: "YRKKH",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, commodi. Beatae optio tempore repellendus praesentium."
+    images: "images/YRKKH img.webp"
+}
+];
